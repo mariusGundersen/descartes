fad6fb2cadadc5b393b889db2778a4e685c20822
Marius Gundersen <me@mariusgundersen.net>
2015-11-29 17:31:51 +0100

    Removed nsp as a build step since the server isn't responding

--- gulpfile.js
+++ gulpfile.js
@@ -57,5 +57,5 @@ gulp.task('clean', function () {
   return del('dist');
 });
 
-gulp.task('prepublish', ['nsp', 'babel']);
+gulp.task('prepublish', [/*'nsp',*/ 'babel']);
 gulp.task('default', ['static', 'test']);
